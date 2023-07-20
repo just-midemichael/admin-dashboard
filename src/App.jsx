cddab72c2e0433d7db9eea3e4b7ee8e8bbc195d9
Olumide Michael <olumide.m.ojo@gmail.com>
2023-07-20 21:14:16 +0100

    fix: lint error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import './App.css'
 
 function App() {
-  
   return (
     <React.Fragment>
-    </React.Fragment>
-  )
-}
 
+    </React.Fragment>
+  );
+}
 export default App
