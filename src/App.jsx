dbb69db83dbb35e0afe82c0c27faee07090c3a98
Olumide Michael <olumide.m.ojo@gmail.com>
2023-07-21 14:05:49 +0100

    feaat: implemented themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import TopBar from './scenes/global/TopBar';
+import { ColorModeContext } from './themes/colorModeContext';
+import { useMode } from './themes/theme';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <React.Fragment>
-      <div className='app'>
-      
-      </div>
-    </React.Fragment>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+          <div className='app'>
+            <main className='content'>
+              <TopBar/>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 export default App
