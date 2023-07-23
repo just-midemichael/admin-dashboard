3e769d34fedc746678eaf8d30b675850710774ed
Olumide Michael <olumide.m.ojo@gmail.com>
2023-07-23 09:36:39 +0100

    feat: add page header comp

--- src/components/admin/header/Header.jsx
+++ src/components/admin/header/Header.jsx
@@ -1,8 +1,28 @@
+import { Box, Typography, useTheme } from "@mui/material"
+import { tokens } from "../../../themes/tonkens"
 
+// eslint-disable-next-line react/prop-types
+const Header = ({title, subtitle}) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
-const Header = () => {
   return (
-    <div>Header</div>
+    <Box m="0 0 30px 0">
+        <Typography 
+            variant="h3" 
+            color={colors.grey[100]} 
+            fontWeight={700} 
+            sx={{ m: "0 0 5px 0"}}
+        >
+            {title}
+        </Typography>
+        <Typography 
+            variant="h5" 
+            color={colors.blueAccent[500]}
+        >
+            {subtitle}
+        </Typography>
+    </Box>
   )
 }
 
